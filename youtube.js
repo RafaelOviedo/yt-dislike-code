cfee7ea2e3d3b5cb12a7de9017ce12650cca31f2
Rafael Oviedo <oviedo.rp@gmail.com>
2022-04-18 08:54:21 -0300

    update(youtube.js, youtube.html): generating code to send comment to specific video

--- youtube.js
+++ youtube.js
@@ -1,16 +1,26 @@
 import axios from 'axios';
-import { channelIdVar, apiKeyVar } from './variables.js'
+import { channelIdVar, apiKeyVar, playlistId } from './variables.js';
 
-
+let scope = 'https://www.googleapis.com/auth/youtube.force-ssl';
 let apiKey = apiKeyVar;
 let channelId = channelIdVar;
+let uploadsId = playlistId;
 
 let response;
 
+// const a = async () => {
+//   response = await axios.get(`https://www.googleapis.com/youtube/v3/playlistItems?playlistId=${uploadsId}&key=${apiKey}&part=snippet&maxResults=15`);
+  
+  
+//   console.log('DATA', response.data.items)
+// }
+
+// a()
 const a = async () => {
-  response = await axios.get(`https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${channelId}&key=${apiKey}`);
-  console.log('RESPONSE', response.data)
+  response = await axios.get(`https://youtube.googleapis.com/youtube/v3/videos/getRating?id=5K06BthMijw&key=AIzaSyBcapKagonajYAjyuuwEFHzJu1GaFmO_R8`);
+  
+  
+  console.log('DATA', response.data)
 }
 
 a()
- 
